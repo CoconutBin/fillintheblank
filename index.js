883fcd05154e952cb3df0d6df258eeff07ff373d
CoconutBin <sea99687@gmail.com>
2023-07-15 00:12:28 +0700

    Added file uploads, redid EventListeners

--- index.js
+++ index.js
@@ -1,48 +1,92 @@
 let config = await fetch(`./config.json`).then(response => response.json())
 let madlib = await fetch(`./prompts/madlib${Math.ceil(Math.random()*5)}.txt`).then(response => response.text())
 
-let customMadLib = document.getElementById("selfmade")
-let normalMadLib = document.getElementById("premade")
-let selfMade = document.getElementById("selfmadelabel")
-let madLibDiv = document.getElementById("madlibconfig")
-let inputMadLib = document.getElementById("custommadlib")
+const customMadLib = document.getElementById("selfmade")
+const normalMadLib = document.getElementById("premade")
+const uploadMadLib = document.getElementById("fileupload")
+const selfMade = document.getElementById("selfmadelabel")
+const fileUpload = document.getElementById("file")
+const madLibDiv = document.getElementById("madlibconfig")
+const inputMadLib = document.getElementById("custommadlib")
 
 customMadLib.addEventListener('change', () => {
-    selfMade.style.display = selfMade.style.display = 'block'
+    fileUpload.style.display = 'none'
+    selfMade.style.display = 'block'
 })
 
 normalMadLib.addEventListener('change', () => {
-    selfMade.style.display = selfMade.style.display = 'none'
+    selfMade.style.display = 'none'
+    fileUpload.style.display = 'none'
 })
 
-madLibDiv.addEventListener('submit', (event) => {
+uploadMadLib.addEventListener('change', () => {
+    fileUpload.style.display = 'block'
+    selfMade.style.display = 'none'
+})
+
+
+madLibDiv.addEventListener('submit',async (event) => {
     event.preventDefault()
+
+    if (uploadMadLib.checked) {
+        const file = fileUpload.files[0];
+        console.log(file.type)
+        if (file.type == 'text/plain') {
+            const text = await file.text();
+            madlib = text;
+        }
+        else throw new Error("Invalid file upload")
+    }
+    
     if(inputMadLib.value != ''){
         madlib = inputMadLib.value
     }
+
+    let instances = 0
+    while(madlib.split('').some(x => x.match(/[${}]/))){
+    madlib = main(madlib)
+    instances++
+    if(instances > 5) break
+    }
+    
+    document.getElementById('finishedmadlib').innerHTML = madlib
+})
+
+function main(text){
     let partsOfSpeech = [
-        ... madlib.split(' ')
+        ... text.split(' ')
         .map(x => x.replace(/[^${\w}]/gi, '')) 
-        .filter(madlib => madlib.match(/\${.{0,}/gi))
+        .filter(x => x.match(/\${.{0,}/gi))
     ]
-    partsOfSpeech = partsOfSpeech.map(x => x.slice(2, -1).replaceAll('_', ' '))
+    .map(x => x.replaceAll(/\${/gi, '')
+    .replaceAll(/}/gi, '')
+    .replaceAll('_', ' '))
+
+
+    console.log(partsOfSpeech, partsOfSpeech.length)
     
     let answerArray = []
+    let instance = 0
     for(let partOfSpeech of partsOfSpeech){
         answerArray.push(prompt(`Give me a ${partOfSpeech}`))
         if(answerArray.some(x => x == null)){
             alert("Seems like an input is null, try again if this isn't intentional")
             break
         }
+        instance++
+        console.log(instance)
     }
     for(let i in partsOfSpeech){
-        madlib = madlib.replace(`\${${partsOfSpeech[i].replaceAll(" ", "_")}}`, answerArray[i] == ''||answerArray[i] == null? config.wordBank[Math.floor(Math.random()*config.wordBank.length)]:answerArray[i])
+        text = 
+        text
+        .replace(`\${${partsOfSpeech[i]
+        .replaceAll(" ", "_")}}`, 
+        answerArray[i] == ''||answerArray[i] == null? 
+        config.wordBank[Math.floor(Math.random()*config.wordBank.length)]:answerArray[i])
     }
-    
-    console.log(madlib)
-    document.getElementById('finishedmadlib').innerHTML = madlib
-})
-
+    console.log(text)
+    return text
+}
 
 
 
